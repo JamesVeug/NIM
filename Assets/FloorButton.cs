7bfeb4756ca5b48765d35d23eea77e882cbeeaeb
James Veugelaers <james.veugelaers@outlook.co.nz>
2016-04-12 09:24:05 +1200

    Improved Camera and added Basic AI
    Improved Camera dynamics
    Added trigger to change camera spectator.
    Added small button for trap door.

---
+++ Assets/FloorButton.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorButton : MonoBehaviour {
+
+    private const int STATE_IDLE = 0;
+    private const int STATE_CLOSING = 1;
+    private const int STATE_CLOSED = 2;
+
+    private int state = STATE_IDLE;
+
+    public float closeDistance = 2f;
+
+    private Vector3 initPos;
+    private Vector3 closePos;
+
+	// Use this for initialization
+	void Start () {
+        initPos = transform.position;
+        closePos = transform.position - new Vector3(0f, closeDistance,0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (state == STATE_CLOSING)
+        {
+            if ((closePos - transform.position).magnitude <= 0.1f)
+            {
+                state = STATE_CLOSED;
+                closed();
+            }
+            else {
+                transform.position += (closePos - initPos) * Time.deltaTime;
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider c)
+    {
+        Debug.LogWarning("COLLIDED");
+        if (c.gameObject.tag == "Player")
+        {
+            Debug.LogWarning("PLAYER");
+            state = STATE_CLOSING;
+        }
+    }
+
+    void closed()
+    {
+        GameObject door = GameObject.Find("TrapDoor");
+        Debug.LogWarning("Door " + door);
+
+        door.transform.Rotate(new Vector3(0, 0, 90));
+    }
+}
---
+++ Assets/Scripts/ChasePlayer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChasePlayer : MonoBehaviour {
+
+    public GameObject whatToChase = null;
+    public Vector3 rotationVector = new Vector3(0f,0f,0f);
+    public float distance = 10f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        Vector3 offset = new Vector3(0, 0, -distance);
+        Vector3 newPos = RotatePointAroundPivot(whatToChase.transform.position+offset, whatToChase.transform.position, rotationVector);
+        this.transform.position = Vector3.Slerp(this.transform.position, newPos, 4 * Time.deltaTime); ;
+
+
+        /*
+        this.transform.position = whatToChase.transform.position + new Vector3(0,distance,0);
+        transform.RotateAround(whatToChase.transform.position, new Vector3(1, 0, 0), rotation.x);
+        transform.RotateAround(whatToChase.transform.position, new Vector3(0, 1, 0), rotation.y);
+        transform.RotateAround(whatToChase.transform.position, new Vector3(0, 0, 1), rotation.z);*/
+
+
+        this.transform.LookAt(whatToChase.transform);
+    }
+
+    public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
+    {
+        Vector3 dir = point - pivot; // get point direction relative to pivot
+        dir = Quaternion.Euler(angles) * dir; // rotate it
+        point = dir + pivot; // calculate rotated point
+        return point; // return it
+    }
+}
