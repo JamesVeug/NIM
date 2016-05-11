68ee71b9d5bb8fd661348a189a541e1afc0ac74e
Carl <narceil@hotmail.com>
2016-05-11 23:26:11 +1200

    show loading screen (however briefly) when loading a new scene

--- Assets/Scripts/UI/UILoadScene.cs
+++ Assets/Scripts/UI/UILoadScene.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class UILoadScene : MonoBehaviour {
 
+	public GameObject loadingScreen;
+
 	public void LoadScene(int level){
-		
+
+		loadingScreen.SetActive (true);
 		Application.LoadLevel (level);
 
 	}
