19d590e97fa05d0ba80f61f6f9584c4720046a50
Matt Puryer-Smith <zynx66@hotmail.co.nz>
2016-05-28 17:14:32 +1200

    Created a checkpoint system that works off the current waypoint system.

---
+++ Assets/Scripts/Matts Scripts/CheckpointPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointPlayer : MonoBehaviour {
+
+    public Checkpoints currentCheckpoint;
+    public Movement movementScript;
+	// Use this for initialization
+	void Start () {
+
+        if (currentCheckpoint == null) {
+            Debug.Log("No checkpoint provided for player");
+        }
+	}
+
+    public void setCheckpoint(Checkpoints checkpoint) {
+        currentCheckpoint = checkpoint;
+        Debug.Log("NEW CHECKPOINT: " + checkpoint.name);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //Debug.Log("TRANSFORM Y: " + this.transform.position.y );
+        if (this.transform.position.y < -10) {
+            transform.position = currentCheckpoint.transform.position;
+            movementScript.currentMovementWaypoint = currentCheckpoint.getWaypoint();
+
+        }
+	}
+}
---
+++ Assets/Scripts/Matts Scripts/Checkpoints.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoints : MonoBehaviour {
+
+    public CheckpointPlayer playerScript;
+    private MovementWaypoint waypoint;
+    // Use this for initialization
+
+    void Start()
+    {
+        waypoint = this.GetComponent<MovementWaypoint>();
+        Debug.Log("WAYPOINT: " + waypoint.ToString());
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TEST1");
+        if (other.gameObject.tag == "Player")
+        {
+            playerScript.setCheckpoint(this);
+        }
+        else {
+            Debug.Log("NO PLAYER TAG FOUND FOR CHECKPOINT");
+        }
+
+
+    }
+
+    public MovementWaypoint getWaypoint() {
+        return waypoint;
+    }
+
+}
