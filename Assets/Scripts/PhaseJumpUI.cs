14244553fe088d137b88c94e83de1bdd12e27f0e
James Veugelaers <james.veugelaers@outlook.co.nz>
2016-05-29 23:36:27 +1200

    Improved Nims glow.

--- Assets/Scripts/PhaseJumpUI.cs
+++ Assets/Scripts/PhaseJumpUI.cs
@@ -10,14 +10,20 @@ public class PhaseJumpUI : MonoBehaviour
     private GameObject staff;
     private GameObject effects;
     private GameObject gem;
+    private GameObject glowObject;
 
-    
+
     public Material standaredMaterial;
     public Material glowMaterial;
     public Material cooldownMaterial;
 
+    private Light light;
     private Text phaseForwardText;
     private Text phasebackwardText;
+    private Image glowImage2;
+    private Image glowImage;
+    private float glowTime = 0f;
+    private bool hasGlown = false;
 
     // Use this for initialization
     void Start()
@@ -33,6 +39,7 @@ public class PhaseJumpUI : MonoBehaviour
                 effects = staff.transform.FindChild("Effects").gameObject;
                 gem = staff.transform.FindChild("MagTealGem").gameObject;
                 gemRenderer = gem.GetComponent<Renderer>();
+                light = effects.transform.FindChild("Light").gameObject.GetComponent<Light>();
             }
         }
 
@@ -42,6 +49,12 @@ public class PhaseJumpUI : MonoBehaviour
 
         Transform backText = FindObjectOfType<Canvas>().transform.FindChild("PhaseBackText");
         if (backText != null) { phasebackwardText = backText.gameObject.GetComponent<Text>(); }
+
+        Transform glowText = FindObjectOfType<Canvas>().transform.FindChild("NimGlow");
+        if (glowText != null) { glowImage = glowText.gameObject.GetComponent<Image>(); glowImage.enabled = false; }
+
+        Transform glowText2 = FindObjectOfType<Canvas>().transform.FindChild("NimGlow2");
+        if (glowText2 != null) { glowImage2 = glowText2.gameObject.GetComponent<Image>(); }
     }
 
     // Update is called once per frame
@@ -51,6 +64,19 @@ public class PhaseJumpUI : MonoBehaviour
         {
             Debug.LogError("No PhaseJump script given.");
             return;
+        }
+        glowTime = Mathf.Max(0, glowTime - Time.deltaTime);
+        glowImage.enabled = glowTime > 0;
+        if( jump.isPhasing())
+        {
+            glowImage.enabled = false;
+            glowImage.transform.localScale = new Vector3(0, 0, 0);
+            glowTime = 0f;
+            glowImage2.enabled = false;
+        }
+        else
+        {
+            glowImage2.enabled = true;
         }
 
         // Show nims phasing status as to if he can phase or not
@@ -99,11 +125,32 @@ public class PhaseJumpUI : MonoBehaviour
 
     public void GrowStuff(bool shouldGlow)
     {
+
         if (effects == null)
         {
             Debug.Log("can not find child Effects in Model!");
             return;
         }
+
+        
+        // Stop glowing
+        if (!shouldGlow && hasGlown && glowTime == 0)
+        {
+            hasGlown = false;
+        }
+
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(gem.transform.position)+new Vector3(0,1,0);
+        glowImage.transform.position = screenPos;
+        glowImage2.transform.position = screenPos;
+        Color c = glowImage.color;
+        c.a = glowTime/2;
+        glowImage.color = c;
+
+
+        // Scale the glow
+        float scale = (1 - glowTime) * 5;
+        glowImage.transform.localScale = new Vector3(scale, scale, scale);
+
 
         // effects
         foreach (Transform child in effects.transform)
@@ -124,15 +171,41 @@ public class PhaseJumpUI : MonoBehaviour
 
             if (!shouldGlow)
             {
-                gemRenderer.material = standaredMaterial; 
+                // No glow
+                gemRenderer.material = standaredMaterial;
+                Color col = glowImage2.color;
+                col.a = 0;
+                light.color = col;
+                glowImage2.color = col;
             }
             else if ( jump.isCoolingDown())
             {
+                // Cooling down
                 gemRenderer.material = cooldownMaterial;
+                Color col = glowImage2.color;
+                col.g = 0;
+                col.b = 0;
+                col.a = 0.5f;
+                glowImage2.color = col;
+                light.color = Color.red;
             }
             else
             {
+                // Glowing 
                 gemRenderer.material = glowMaterial;
+                Color col = glowImage2.color;
+                col.g = 255;
+                col.b = 255;
+                col.a = 0.5f;
+                glowImage2.color = col;
+
+                light.color = Color.white;
+
+                if (shouldGlow && !hasGlown)
+                {
+                    glowTime = 1;
+                    hasGlown = true;
+                }
             }
         }
         else
