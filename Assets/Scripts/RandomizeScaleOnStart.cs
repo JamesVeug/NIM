1c41dfe65d2b24f0e9c8872aa58f00fbd15c0d02
James Veugelaers <james.veugelaers@outlook.co.nz>
2016-05-29 12:54:28 +1200

    Environment Prefabs
    Added Environment Prefabs to be placed around the level.

---
+++ Assets/Scripts/RandomizeScaleOnStart.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomizeScaleOnStart : MonoBehaviour {
+
+
+    public float minXScale = 1;
+    public float maxXScale = 1;
+    public float minYScale = 1;
+    public float maxYScale = 1;
+    public float minZScale = 1;
+    public float maxZScale = 1;
+
+    // Use this for initialization
+    void Start () {
+        transform.localScale = new Vector3(Random.Range(minXScale, maxXScale), Random.Range(minYScale, maxYScale), Random.Range(minZScale, maxZScale));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
